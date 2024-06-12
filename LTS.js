92737693d140b95ca89f9cc9ae0e994d7cdda28b
bennie_chen <>
2024-06-12 13:18:43 +0800

    Revert "修改完场状态"
    This reverts commit b3cf47b17a403acbcebe660fb59c7fde0ec6f507.

--- LTS.js
+++ LTS.js
@@ -1,15 +1,14 @@
-// quantumult_response_rewriter.js
+// 解析响应体
 let body = $response.body;
 let obj = JSON.parse(body);
 
-if (obj.data) {
-    if (obj.data.purchased) {
-        obj.data.purchased = ["3"];
-    }
-    if (obj.data.stateName) {
-        obj.data.stateName = "完场";
-    }
+// 修改 "purchased" 字段为 ["3"]
+if (obj.data && obj.data.purchased) {
+  obj.data.purchased = ["3"];
 }
 
+// 转换回 JSON 字符串
 body = JSON.stringify(obj);
+
+// 返回修改后的响应体
 $done({body});
